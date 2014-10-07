58db34bfd67e3f0f64295eeb0e9a8a5c100c0fdf
ananogal <ana.nogal@gmail.com>
2014-10-07 18:48:06 +0100

    organize all files.

---
+++ app/server.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'data_mapper'
+require 'rack-flash'
+
+require_relative 'models/link'
+require_relative 'models/tag'
+require_relative 'models/user'
+require_relative 'data_mapper_setup'
+require_relative 'controllers/application'
+require_relative 'controllers/links'
+require_relative 'controllers/tags'
+require_relative 'controllers/users'
+require_relative 'controllers/sessions'
+require_relative 'helpers/application'
+
+use Rack::Flash, :sweep =>true
+enable :sessions
+
+
+
